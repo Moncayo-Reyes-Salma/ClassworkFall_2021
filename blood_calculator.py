74379fff65a6df803e888e1d420507e065248db5
Salma Moncayo-Reyes <salma.moncayoreyes@duke.edu>
2021-08-30 12:44:32 -0400

    Added blood calculator

---
+++ blood_calculator.py
@@ -0,0 +1,10 @@
+#blood_calculator.py
+def interface() :
+    print ("Blood Calcualtor")
+    print ("Make choice")
+    print("9 - Quit")
+    choice = input("Make a choice: ")
+    print (choice)
+    return choice
+
+interface()
